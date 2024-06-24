6cfe0bf45df067b85ab91b51f88d9c5a2767f33e
hubrygo <hbrygo05@gmail.com>
2024-06-24 14:19:13 +0200

    pas la bonne page mais ca run OUIIIIII

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,15 +1,31 @@
-service mysql start;
+#!/bin/sh
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+# start maria db service
+service mariadb start
 
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+# execute next commands as root
+mariadb -v -u root <<EOF
 
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';
 
-mysql -e "FLUSH PRIVILEGES;"
+GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%';
 
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO 'root'@'%' IDENTIFIED BY '$SQL_ROOT_PASSWORD';
 
-exec mysqld_safe
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$SQL_ROOT_PASSWORD');
+
+FLUSH PRIVILEGES;
+
+EOF
+
+# wait 5 sec that everything is done 
+sleep 5
+
+# stops maria db service , needs to be started from docker compose
+service mariadb stop
+
+# Exécute all comands fron file
+exec "$@"
+
